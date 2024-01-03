b3f18812b77c0dc556f97c0b60a466c5e4f926a7
Herbert Henrique <herbert.henrique@sumup.com>
2024-01-03 15:35:11 +0100

    feat: make vercel and prisma compatible3

--- app/lib/database/prisma.ts
+++ app/lib/database/prisma.ts
@@ -1,16 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-// Avoid instantiating too many instances of Prisma in development
-// https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
+
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
